734d54bd0168135a76a87982e81b1bbd9912a974
KWeatherwalks <kevin.weatherwalks@gmail.com>
2023-02-12 14:01:28 -0700

    implement insertion method

--- hashtable.py
+++ hashtable.py
@@ -1,9 +1,13 @@
 # hashtable.py
 
+
 class HashTable:
     def __init__(self, capacity):
         self.values = capacity * [None]
 
     def __len__(self):
         return len(self.values)
-    
+
+    def __setitem__(self, key, value):
+        index = hash(key) % len(self)
+        self.values[index] = value
--- test_hashtable.py
+++ test_hashtable.py
@@ -1,5 +1,7 @@
 # test_hashtable.py
 
+import pytest
+
 from hashtable import HashTable
 
 
@@ -9,6 +11,7 @@ def test_should_create_hashtable():
 
 def test_should_report_capacity():
     assert len(HashTable(capacity=100)) == 100
+
 
 def test_should_create_empty_value_slots():
     # Given
@@ -20,3 +23,22 @@ def test_should_create_empty_value_slots():
 
     # Then
     assert actual_values == expected_values
+
+
+def test_should_insert_key_value_pairs():
+    hash_table = HashTable(capacity=100)
+
+    hash_table["hola"] = "hello"
+    hash_table[98.6] = 37
+    hash_table[False] = True
+
+    assert "hello" in hash_table.values
+    assert 37 in hash_table.values
+    assert True in hash_table.values
+
+    assert len(hash_table) == 100
+
+
+@pytest.mark.skip
+def test_should_not_shrink_when_removing_elements():
+    pass
