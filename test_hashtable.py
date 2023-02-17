e9d14ebf1735fe815da41b09e45d0e502e39cd07
KWeatherwalks <kevin.weatherwalks@gmail.com>
2023-02-16 18:49:56 -0700

    implement reporting of hash table length

--- test_hashtable.py
+++ test_hashtable.py
@@ -6,21 +6,30 @@ from pytest_unordered import unordered
 from hashtable import HashTable
 
 
+@pytest.fixture
+def hash_table():
+    sample_data = HashTable(capacity=100)
+    sample_data["hola"] = "hello"
+    sample_data[98.6] = 37
+    sample_data[False] = True
+    return sample_data
+
+
 def test_should_create_hashtable():
     assert HashTable(capacity=100) is not None
 
 
-def test_should_report_capacity():
-    assert len(HashTable(capacity=100)) == 100
+def test_should_report_length_of_empty_hash_table():
+    assert len(HashTable(capacity=100)) == 0
 
 
-def test_should_create_empty_value_slots():
+def test_should_create_empty_pair_slots():
     # Given
     expected_values = [None, None, None]
     hash_table = HashTable(capacity=3)
 
     # When
-    actual_values = hash_table._pairs
+    actual_values = hash_table._slots
 
     # Then
     assert actual_values == expected_values
@@ -37,7 +46,7 @@ def test_should_insert_key_value_pairs():
     assert (98.6, 37) in hash_table.pairs
     assert (False, True) in hash_table.pairs
 
-    assert len(hash_table) == 100
+    assert len(hash_table) == 3
 
 
 @pytest.mark.skip
@@ -53,15 +62,6 @@ def test_should_insert_none_value():
     hash_table = HashTable(capacity=100)
     hash_table["key"] = None
     assert ("key", None) in hash_table.pairs
-
-
-@pytest.fixture
-def hash_table():
-    sample_data = HashTable(capacity=100)
-    sample_data["hola"] = "hello"
-    sample_data[98.6] = 37
-    sample_data[False] = True
-    return sample_data
 
 
 def test_should_find_value_by_key(hash_table):
@@ -104,13 +104,13 @@ def test_should_get_value_with_default(hash_table):
 def test_should_delete_key_value_pair(hash_table):
     assert "hola" in hash_table
     assert ("hola", "hello") in hash_table.pairs
-    assert len(hash_table) == 100
+    assert len(hash_table) == 3
 
     del hash_table["hola"]
 
     assert "hola" not in hash_table
     assert ("hola", "hello") not in hash_table.pairs
-    assert len(hash_table) == 100
+    assert len(hash_table) == 2
 
 
 def test_should_raise_key_error_when_deleting(hash_table):
@@ -127,7 +127,7 @@ def test_should_update_value(hash_table):
     assert hash_table["hola"] == "hallo"
     assert hash_table[98.6] == 37
     assert hash_table[False] is True
-    assert len(hash_table) == 100
+    assert len(hash_table) == 3
 
 
 def test_should_return_pairs(hash_table):
@@ -183,3 +183,25 @@ def test_should_convert_to_dict(hash_table):
     assert set(dictionary.keys()) == hash_table.keys
     assert set(dictionary.items()) == hash_table.pairs
     assert list(dictionary.values()) == unordered(hash_table.values)
+
+
+def test_should_not_create_hashtable_with_zero_capacity():
+    with pytest.raises(ValueError):
+        HashTable(capacity=0)
+
+
+def test_should_not_create_hashtable_with_negative_capacity():
+    with pytest.raises(ValueError):
+        HashTable(capacity=-100)
+
+
+def test_should_report_length(hash_table):
+    assert len(hash_table) == 3
+
+
+def test_should_report_capacity_of_empty_hash_table():
+    assert HashTable(capacity=100).capacity == 100
+
+
+def test_should_report_capacity(hash_table):
+    assert hash_table.capacity == 100
